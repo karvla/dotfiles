b97db5f510a8beae2b78c4545c617becfe83434b
Arvid Larsson <arvid94@gmail.com>
2022-01-14 09:46:51 +0100

    add conda_init alias

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,3 +2,5 @@ export PATH="$HOME/.poetry/bin:$PATH"
 #set PATH /usr/local/bin $PATH
 set -U fish_user_paths ~/bin /opt/bin
 
+alias conda_init="/opt/anaconda/condabin/conda "shell.fish" "hook" $argv | source"
+alias vim="nvim"
